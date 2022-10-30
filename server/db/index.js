fd1e168067a3328ea8e771850e07c60ac8f8029a
Marian Neagu <marianmadalinneagu@gmail.com>
2022-10-30 16:46:52 +0200

    post /save-trip trip and trip_activity
    Signed-off-by: Marian Neagu <marianmadalinneagu@gmail.com>

--- server/db/index.js
+++ server/db/index.js
@@ -1,8 +1,8 @@
-import * as dotnev from 'dotenv'
-import pg from 'pg'
+import * as dotnev from "dotenv";
+import pg from "pg";
 
 dotnev.config({
-  path: '../.env'
+  path: "../.env",
 });
 
 const { Pool } = pg;
@@ -13,24 +13,24 @@ const dbConfig = {
   database: process.env.POSTGRES_DB,
   password: process.env.POSTGRES_PASSWORD,
   port: process.env.POSTGRES_PORT,
-}
+};
 
 export const pool = new Pool(dbConfig);
 
 try {
   await new Promise((resolve, reject) => {
-    pool.query('SELECT NOW()', (err, res) => {
+    pool.query("SELECT NOW()", (err, res) => {
       if (err) {
         return reject(err);
       }
 
       resolve(res);
     });
-  })
+  });
 
-  console.log('Successfully connected to the database.');
+  console.log("Successfully connected to the database.");
 } catch (err) {
-  console.error('An error occurred while connecting to the database');
+  console.error("An error occurred while connecting to the database");
   pool.end();
   process.exit(1);
 }
--- server/index.js
+++ server/index.js
@@ -1,14 +1,58 @@
-import express from 'express'
-import { pool } from './db/index.js'
+import express from "express";
+import { pool } from "./db/index.js";
 
 const PORT = 8080;
 
 const app = express();
 
-app.get('/test', (req, res) => {
-  res.json({ msg: 'hello!' });
+app.use(express.json());
+
+app.get("/test", (req, res) => {
+  res.json({ msg: "hello!" });
 });
 
 app.listen(PORT, () => console.log(`Server up and running on port ${PORT}`));
 
-console.log('pool');
+app.post("/save-trip", async (req, rest) => {
+  const body = req.body;
+  console.log(body);
+
+  // insert new trip
+  const client = await pool.connect();
+  console.log(body.trip.start_date);
+  let values = [
+    body.trip.start_date,
+    body.trip.end_date,
+    body.trip.organiser_id,
+  ];
+  try {
+    let res = await client.query(
+      "INSERT INTO trip(start_date, end_date, organiser_id) VALUES($1, $2, $3) RETURNING id",
+      values
+    );
+    const tripId = res.rows[0].id;
+
+    // insert in trip activity
+    for (let i = 0; i < body.activities.length; i++) {
+      values = [
+        tripId,
+        body.activities[i].activity_id,
+        body.activities[i].day_number,
+        body.activities[i].order_number,
+        body.activities[i].visiting_time,
+        body.activities[i].note,
+      ];
+      res = await client.query(
+        "INSERT INTO trip_activity(trip_id, activity_id, day_number, order_number, visiting_time, note) VALUES($1, $2, $3, $4, $5, $6)",
+        values
+      );
+      values = [];
+    }
+  } catch (err) {
+    console.log(err);
+  } finally {
+    client.release();
+  }
+});
+
+console.log("pool");
