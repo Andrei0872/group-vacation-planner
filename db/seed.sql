ff20333da8596963fcd02dae8c3c928ef9d20d95
Andrei Gatej <andreigtj01@gmail.com>
2022-10-30 16:56:16 +0200

    feat(db): add seed data

---
+++ db/seed.sql
@@ -0,0 +1,114 @@
+-- user
+INSERT INTO "user" (username, password, email)
+VALUES ('patricia', 'patricia123', 'patricia@gmail.com');
+INSERT INTO "user" (username, password, email)
+VALUES ('andrei', 'andrei123', 'andrei@gmail.com');
+INSERT INTO "user" (username, password, email)
+VALUES ('marian', 'marian123', 'marian@gmail.com');
+-- activity
+INSERT INTO activity (name, city, country, category)
+VALUES (
+        'Muzeul de Arta Contemporana',
+        'Bucuresti',
+        'Romania',
+        '{cultural, indoor}'
+    );
+INSERT INTO activity (name, city, country, category)
+VALUES (
+        'Gradina Botanica',
+        'Bucuresti',
+        'Romania',
+        '{cultural, outdoor}'
+    );
+INSERT INTO activity (name, city, country, category)
+VALUES (
+        'Arena Nationala',
+        'Bucuresti',
+        'Romania',
+        '{sport, outdoor}'
+    );
+INSERT INTO activity (name, city, country, category)
+VALUES (
+        'Zest Pizza',
+        'Bucuresti',
+        'Romania',
+        '{food, drinks}'
+    );
+INSERT INTO activity (name, city, country, category)
+VALUES (
+        'Sala Palatului',
+        'Bucuresti',
+        'Romania',
+        '{entertainment, indoor}'
+    );
+INSERT INTO activity (name, city, country, category)
+VALUES (
+        'AFI Cotroceni',
+        'Bucuresti',
+        'Romania',
+        '{shopping, indoor}'
+    );
+
+INSERT INTO activity (name, city, country, category)
+VALUES (
+        'Parcul Circului',
+        'Bucuresti',
+        'Romania',
+        '{sport, outdoor}'
+    );
+INSERT INTO activity (name, city, country, category)
+VALUES (
+        'Ateneul Roman',
+        'Bucuresti',
+        'Romania',
+        '{cultural, indoor}'
+    );
+INSERT INTO activity (name, city, country, category)
+VALUES (
+        'Tenis Club',
+        'Bucuresti',
+        'Romania',
+        '{sport, outdoor}'
+    );
+INSERT INTO activity (name, city, country, category)
+VALUES (
+        'Taverna Racilor',
+        'Bucuresti',
+        'Romania',
+        '{food, drinks, indoor}'
+    );
+INSERT INTO activity (name, city, country, category)
+VALUES (
+        'The Vintage',
+        'Bucuresti',
+        'Romania',
+        '{drinks, entertainment, indoor}'
+    );
+INSERT INTO activity (name, city, country, category)
+VALUES (
+        'Opera Nationala',
+        'Bucuresti',
+        'Romania',
+        '{cultural, indoor}'
+    );
+INSERT INTO activity (name, city, country, category)
+VALUES (
+        'Teatrul de comedie',
+        'Bucuresti',
+        'Romania',
+        '{cultural, entertainment, indoor}'
+    );
+INSERT INTO activity (name, city, country, category)
+VALUES (
+        'Sun Plaza',
+        'Bucuresti',
+        'Romania',
+        '{shopping, indoor}'
+    );
+INSERT INTO activity (name, city, country, category)
+VALUES (
+        'Oraselul Copiilor',
+        'Bucuresti',
+        'Romania',
+        '{sport, outdoor}'
+    );
